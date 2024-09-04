86191f6709837070c8b61464528e6e1c1ec50fb9
souta1007 <22ts086@m.nanzan-u.ac.jp>
2024-09-04 18:08:03 +0900

    ADD ブログ投稿作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -18,6 +19,12 @@ class PostController extends Controller
     public function create(Post $post)
     {
         return view('posts.create');
+    }
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request[ 'post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
 ?>
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\PostController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/',[postController::class, 'index']);
+Route::get('/',[PostController::class, 'index']);
 Route::get('/posts/create',[PostController::class,'create']);
-Route::get('/posts/{post}', [postController::class, 'show']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
